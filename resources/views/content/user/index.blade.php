a3b6ee7054a16c056eb3c50edc77e734868e3c1b
dennisismail608-star <dennisismail608>
2025-09-11 14:52:17 +0700

    user index & user controller

---
+++ resources/views/content/user/index.blade.php
@@ -0,0 +1,37 @@
+@extends('index')
+@section('content')
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Tambah User</h4>
+                        <p class="card-description"></p>
+                        <div class="table-responsive">
+                            <div class="table">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>email</th>
+                                        <th>level</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('content/dashboard', [HomeController::class, 'index'])->name('content.dashboard');
+route::resource('user', UserController::class);
